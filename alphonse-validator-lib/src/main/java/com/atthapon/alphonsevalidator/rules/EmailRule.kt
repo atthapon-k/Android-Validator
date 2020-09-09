51fdb327b48817c95f9a70ed1ef53719a138c604
atthapon-k <polcpekusrc@gmail.com>
2020-09-09 17:59:27 +0700

    remove internal access for extension

--- alphonse-validator-lib/src/main/java/com/atthapon/alphonsevalidator/rules/EmailRule.kt
+++ alphonse-validator-lib/src/main/java/com/atthapon/alphonsevalidator/rules/EmailRule.kt
@@ -5,7 +5,12 @@ import com.atthapon.alphonsevalidator.Validator
 class EmailRule(var errorMsg: String? = null): BaseRule {
     override fun validate(text: String): Boolean {
         return Validator(text).regex(
-            "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z‌​]{2,})$"
+            "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                    + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                    + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                    + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
         ).check()
     }
 
